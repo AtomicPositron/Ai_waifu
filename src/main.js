c0501228b98b0044bc47e8f86c636f8a8d68da85
AtomicPositron <ietorobong@gmail.com>
2025-08-04 20:22:15 +0100

    added inject

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import VueAutoscroll from '@codekraft-studio/vue-autoscroll'
-
+import { inject } from '@vercel/analytics';
+ 
+inject();
 const app = createApp(App)
 app.use(VueAutoscroll)
 app.mount('#app')
